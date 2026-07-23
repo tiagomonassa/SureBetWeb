e68a51ea364479de9d530bca3b3d81bc2b00a13f
tiagomonassa <tiagomonassa@gmail.com>
2026-07-22 21:06:54 -0300

    Corrige dependencias JWT e URL API producao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@
 
 const API_URL =
     import.meta.env.VITE_API_URL ||
-    "https://surebetweb.onrender.com";
+    "https://surebets-ve2i.onrender.com/";
 
 console.log("API_URL USADA:", API_URL);
 
